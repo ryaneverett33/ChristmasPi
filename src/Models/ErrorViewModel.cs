c06030639e0ae62191772fe4a4fa0e92f9ef4127
Ryan Everett <ryaneverett@outlook.com>
2019-08-22 15:15:28 -0400

    Add ASP.NET Skeleton

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChristmasPi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
