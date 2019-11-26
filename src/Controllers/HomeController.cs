8a067bb9a9b66a4c2121b529987766c1990044cd
Ryan Everett <ryaneverett33@outlook.com>
2019-11-25 22:45:16 -0500

    Working on frontend, now serving paper-kit by default

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ChristmasPi.Models;
+using ChristmasPi.Operations;
+using ChristmasPi.Operations.Interfaces;
+using ChristmasPi.Animation;
 
 namespace ChristmasPi.Controllers {
     [Route("/")]
@@ -16,6 +19,21 @@ namespace ChristmasPi.Controllers {
 
         public IActionResult Privacy() {
             return View();
+        }
+
+        [HttpGet("solid")]
+        public IActionResult Solid() {
+            return View();
+        }
+
+        [HttpGet("animation")]
+        public IActionResult Animation() {
+            var model = new AnimationModel {
+                Disabled = (OperationManager.Instance.CurrentOperatingMode is IOffMode),
+                Animations = AnimationManager.Instance.GetAnimations(),
+                CurrentAnimation = ""
+            };
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/Models/AnimationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChristmasPi.Models {
+    public class AnimationModel {
+        public bool Disabled { get; set; }
+        public string[] Animations { get; set; }
+        public string CurrentAnimation { get; set; }
+        public PlayState CurrentState { get; set; }
+    }
+    public enum PlayState {
+        Playing,
+        Paused,
+        Stopped
+    }
+}
